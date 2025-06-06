2ab2ff675d4f4c9ab5187fbf328e070590235f07
Melvin Calel <msksaul@gmail.com>
2025-06-06 17:32:43 -0600

    Routes

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from 'express'
 
 import { PORT } from './config/env'
 
+import userRouter from './routes/user.routes'
+import authRouter from './routes/auth.routes'
+import subscriptionRouter from './routes/subscription.routes'
+
 const app = express()
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API!')
