e131bf7a3f70c0296ed0622444df1e9537d405d4
Melvin Calel <msksaul@gmail.com>
2025-06-09 14:40:27 -0600

    Error Handler

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ import userRouter from './routes/user.routes'
 import authRouter from './routes/auth.routes'
 import subscriptionRouter from './routes/subscription.routes'
 import connectToDatabase from './database/mongodb'
+import errorMiddleware from './middlewares/error.middleware'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API!')
