aba0e9a16be6dd231ec2febe77277d9cfaf19b20
Melvin Calel <msksaul@gmail.com>
2025-06-11 15:04:01 -0600

    Reminder Workflow

--- config/env.js
+++ config/env.js
@@ -4,10 +4,13 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
 
 export const { 
   PORT,
+  SERVER_URL,
   NODE_ENV,
   DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
   ARCJET_KEY,
-  ARCJET_ENV
+  ARCJET_ENV,
+  QSTASH_TOKEN,
+  QSTASH_URL
 } = process.env
